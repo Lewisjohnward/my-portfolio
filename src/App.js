d97393b2a2cb3ba6e20aa40346f1d3a9f538cc60
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-08-10 11:25:14 +0100

    updated icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import {GlobalStyle} from "./globalstyles"
 import styled from "styled-components"
 import {VscGithub} from "react-icons/vsc"
+import {Javascript, C, Html, CSS} from "./assets/icos/Icos"
+import express from "./assets/icos/express.png"
+import mongo from "./assets/icos/mongo.png"
+import node from "./assets/icos/node.png"
+import react from "./assets/icos/react.png"
 
 const Page = styled.div`
     margin: 100px 400px;
@@ -19,7 +24,8 @@ const Title = styled.h1`
     align-self: flex-end;
 `
 
-const Text = styled.text`
+const Text = styled.p`
+    margin-bottom: 50px;
 `
 
 const IcoContainer = styled.div`
@@ -44,6 +50,21 @@ const BodyContainer = styled.div`
     padding-right: 400px;
 `
 
+const TechContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    * {
+        padding: 5px;
+    }
+
+`
+const Img = styled.img`
+    width: 70px;
+`
+
+
 const App = () => {
     return (
         <Page>
@@ -58,8 +79,20 @@ const App = () => {
             </HeaderContainer>
             <BodyContainer>
                 <Section>
-                <SubTitle>About</SubTitle>
-                <Text> A self-taught Front End Developer with experience building Vanilla and React.js websites. With a background in both UI/UX and low-level programming, I can develop websites from scratch and raise them into modern, responsive, user-friendly, and dependable web experiences.</Text>
+                    <SubTitle>About</SubTitle>
+                    <Text> A self-taught Front End Developer with experience building Vanilla and React.js websites. With a background in both UI/UX and low-level programming, I can develop websites from scratch and raise them into modern, responsive, user-friendly, and dependable web experiences.</Text>
+                    <SubTitle>Technologies</SubTitle>
+                    <TechContainer>
+                        <Javascript />
+                        <C />
+                        <CSS />
+                        <Html />
+                        <Img src={express} />
+                        <Img src={react} />
+                        <Img src={node} />
+                        <Img src={mongo} />
+                    </TechContainer>
+                    <SubTitle>Projects</SubTitle>
                 </Section>
             </BodyContainer>
         </Page>
